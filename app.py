f1a1e52f2a115ef0613a656fa688768551fca712
haiaza <ajames1342@gmail.com>
2025-02-05 06:06:28 -0500

    Progress update.

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     # repalce hardcoding
     return render_template('home.html')
+
+@app.route('/wishlist')
+def load_wishlist():
+    # repalce hardcoding
+    return render_template('wishlist.html')
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug = True)
